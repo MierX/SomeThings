b4952731a2372e01faa56db11401a21e9557d43d
MierX <1214915547@qq.com>
2023-02-01 17:26:50 +0800

    0118

--- Study/Golang/src/2023/0118/08递归函数.go
+++ Study/Golang/src/2023/0118/08递归函数.go
@@ -7,7 +7,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // 递归函数
 // 没有出口的递归函数是死递归 可能导致程序崩溃
@@ -20,6 +23,30 @@ func demo4(n int) {
 	demo4(n)
 }
 
-func main() {
+func main0801() {
 	demo4(10)
+}
+
+// 计算一个数的阶乘
+
+var sum1 = 1
+var sum1str = ""
+
+func demo5(n int) {
+	if n == 1 {
+		return
+	}
+
+	demo5(n - 1)
+	if sum1str == "" {
+		sum1str = strconv.Itoa(sum1) + " * " + strconv.Itoa(n)
+	} else {
+		sum1str = sum1str + " * " + strconv.Itoa(n)
+	}
+	sum1 *= n
+}
+
+func main() {
+	demo5(6)
+	fmt.Println(sum1str + " = " + strconv.Itoa(sum1))
 }
