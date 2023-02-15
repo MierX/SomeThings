c7abb3c46b11712a093c3ddf2d062149e9d40212
MierX <1214915547@qq.com>
2023-02-15 17:59:49 +0800

    0215

---
+++ Study/Golang/src/2023/0215/01第一个Go程序.go
@@ -0,0 +1,14 @@
+/*
+ * @Author MierX
+ * @Title 01第一个Go程序
+ * @Date 2023-02-15 周一
+ * @Time 17:30:55
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
---
+++ Study/Golang/src/2023/0215/02命令行运行程序.go
@@ -0,0 +1,14 @@
+/*
+ * @Author MierX
+ * @Title 02命令行运行程序
+ * @Date 2023-02-15 周一
+ * @Time 17:52:26
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("你好")
+}
